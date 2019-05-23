9091fe9613ea85d0d6e3132e55dc4940144d8796
Brandon.Lundt <brandon.lundt@plexus.com>
2019-05-22 22:07:53 -0500

    Initial commit of project data

---
+++ InvokeGodHammerCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace PandorasBox
+{
+    [Cmdlet(VerbsLifecycle.Invoke,"GodHammer")]
+    public class InvokeGodHammerCommand : PSCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ParameterSetName = "Path",
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public string Path { get; set; }
+
+        // This method gets called once for each cmdlet in the pipeline when the pipeline starts executing
+        protected override void BeginProcessing()
+        {
+            WriteVerbose("Begin!");
+        }
+
+        // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
+        protected override void ProcessRecord()
+        {
+            
+        }
+
+        // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
+        protected override void EndProcessing()
+        {
+            WriteVerbose("End!");
+        }
+    }
+}
