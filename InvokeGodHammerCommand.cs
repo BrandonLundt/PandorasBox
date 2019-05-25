94171b267e25be35a62b8743da4e2c65274f751b
Brandon Lundt <brandon.lundt@plexus.com>
2019-05-25 06:47:18 -0500

    Started working through recursive folder logic

--- InvokeGodHammerCommand.cs
+++ InvokeGodHammerCommand.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.IO;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 
@@ -14,7 +16,14 @@ namespace PandorasBox
             ValueFromPipeline = true,
             ValueFromPipelineByPropertyName = true)]
         public string Path { get; set; }
-
+        [Parameter(
+            Mandatory = true,
+            Position = 1,
+            ParameterSetName = "Path",
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public string Pattern { get; set; }
+        private Queue directoriesToProcess;
         // This method gets called once for each cmdlet in the pipeline when the pipeline starts executing
         protected override void BeginProcessing()
         {
@@ -24,7 +33,26 @@ namespace PandorasBox
         // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
         protected override void ProcessRecord()
         {
+            DirectoryInfo di = new DirectoryInfo( Path);
             
+            Console.WriteLine("The directory {0} contains the following files:", di.Name);
+            foreach (FileInfo f in fiArr)
+            {
+                Console.WriteLine("The size of {0} is {1} bytes.", f.Name, f.Length);
+                f.Delete();
+            }
+
+            directoriesToProcess.Enqueue(new DirectoryInfo( Path));
+            while( directoriesToProcess.Count > 0  )
+            {
+                try
+                {
+                    DirectoryInfo currentDirectory = (DirectoryInfo) directoriesToProcess.Dequeue();
+                    directoriesToProcess.Enqueue( currentDirectory.GetDirectories());
+                    FileInfo[] fiArr = currentDirectory.GetFiles();
+                }
+                catch{}
+            }
         }
 
         // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
